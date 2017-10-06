021a1f2b47a261a559b282195d838418823dd9b9
Robbendebiene <robbendebiene@mailbox.org>
2017-10-06 16:32:38 +0200

    added backup, restore and reset; added new actions: screen shot, copy url, copy selection, copy image; added advanced settings: new tab position, remove tab focus, zoom step; removed new tab after and new tab right; enhanced link to bookmark, open background link, zoom in/out behavior

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -17,23 +17,17 @@ let Actions = {
     chrome.tabs.duplicate(this.id);
   },
 
-  NewTab: function () {
-    chrome.tabs.create({
-      active: true
-    })
-  },
+  NewTab: function (data) {
+    let index = null;
 
-  NewTabAfter: function () {
+    if (data.newTabPosition === "after")
+      index = this.index + 1;
+    else if (data.newTabPosition === "before")
+      index = this.index;
+
     chrome.tabs.create({
       active: true,
-      index: this.index + 1
-    })
-  },
-
-  NewTabBefore: function () {
-    chrome.tabs.create({
-      active: true,
-      index: this.index
+      index: index
     })
   },
 
@@ -45,8 +39,32 @@ let Actions = {
     chrome.tabs.reload(this.id, { bypassCache: true });
   },
 
-  Remove: function () {
-    chrome.tabs.remove(this.id);
+  Remove: function (data) {
+    chrome.tabs.query({
+      windowId: this.windowId
+    }, (tabs) => {
+      // if there are other tabs to focus
+      if (tabs.length > 1) {
+        let nextTab = null, index;
+        // get right or left tab if existing
+        if (data.removeTabFocus === "right" || data.removeTabFocus === "left") {
+          if (data.removeTabFocus === "right")
+            index = this.index < tabs.length - 1 ? this.index + 1 : this.index - 1;
+          else
+            index = this.index > 0 ? this.index - 1 : this.index + 1;
+          nextTab = tabs.find((element) => element.index === index);
+        }
+        // get the previous tab
+        else if (data.removeTabFocus === "previous") {
+          nextTab = tabs.reduce((previous, current) => {
+            return previous.lastAccessed > current.lastAccessed || current.active ? previous : current;
+          });
+        }
+        if (nextTab) chrome.tabs.update(nextTab.id, { active: true });
+      }
+      // remove tab
+      chrome.tabs.remove(this.id);
+    });
   },
 
   RemoveRight: function () {
@@ -54,10 +72,11 @@ let Actions = {
       currentWindow: true,
       pinned: false
     }, (tabs) => {
-      for (let i = tabs.length - 1; i >= 0; i--) {
-        if (tabs[i].index <= this.index) break;
-        chrome.tabs.remove(tabs[i].id);
-      }
+      // filter all tabs to the right
+      tabs = tabs.filter((tab) => tab.index > this.index);
+      // create array of tap ids
+      tabs = tabs.map((tab) => tab.id);
+      chrome.tabs.remove(tabs);
     });
   },
 
@@ -66,21 +85,23 @@ let Actions = {
       currentWindow: true,
       pinned: false
     }, (tabs) => {
-      for (let i = 0; i < tabs.length; i++) {
-        if (tabs[i].index >= this.index) break;
-        chrome.tabs.remove(tabs[i].id);
-      }
+      // filter all tabs to the left
+      tabs = tabs.filter((tab) => tab.index < this.index);
+      // create array of tap ids
+      tabs = tabs.map((tab) => tab.id);
+      chrome.tabs.remove(tabs);
     });
   },
 
   RemoveOther: function () {
     chrome.tabs.query({
       currentWindow: true,
-      pinned: false
+      pinned: false,
+      active: false
     }, (tabs) => {
-      for (let i = 0; i < tabs.length; i++) {
-        if (tabs[i].index !== this.index) chrome.tabs.remove(tabs[i].id);
-      }
+      // create array of tap ids
+      tabs = tabs.map((tab) => tab.id);
+      chrome.tabs.remove(tabs);
     });
   },
 
@@ -90,16 +111,26 @@ let Actions = {
     });
   },
 
-  ZoomIn: function () {
+  ZoomIn: function (data) {
+    let zoomLevels = [.3, .5, .67, .8, .9, 1, 1.1, 1.2, 1.33, 1.5, 1.7, 2, 2.4, 3];
+
     chrome.tabs.getZoom(this.id, (z) => {
-      z = Math.min(3, z * 2);
+      if (data.zoomStep)
+        z = Math.min(3, z + data.zoomStep/100);
+      else
+        z = zoomLevels.find((element) => element > z) || 3;
       chrome.tabs.setZoom(this.id, z);
     });
   },
 
-  ZoomOut: function () {
+  ZoomOut: function (data) {
+    let zoomLevels = [3, 2.4, 2, 1.7, 1.5, 1.33, 1.2, 1.1, 1, .9, .8, .67, .5, .3];
+
     chrome.tabs.getZoom(this.id, (z) => {
-      z = Math.max(0.3, z / 2);
+      if (data.zoomStep)
+        z = Math.max(.3, z - data.zoomStep/100);
+      else
+        z = zoomLevels.find((element) => element < z) || .3;
       chrome.tabs.setZoom(this.id, z);
     });
   },
@@ -191,7 +222,9 @@ let Actions = {
     }, (tabs) => {
       let index = this.index + 1;
       if (index >= tabs.length) index = 0;
-      chrome.tabs.update(tabs[index].id, { active: true });
+
+      let tab = tabs.find((element) => element.index === index);
+      chrome.tabs.update(tab.id, { active: true });
     });
   },
 
@@ -201,7 +234,9 @@ let Actions = {
     }, (tabs) => {
       let index = this.index - 1;
       if (index < 0) index = tabs.length - 1;
-      chrome.tabs.update(tabs[index].id, { active: true });
+
+      let tab = tabs.find((element) => element.index === index);
+      chrome.tabs.update(tab.id, { active: true });
     });
   },
 
@@ -284,42 +319,87 @@ let Actions = {
   },
 
   ImageToTab: function (data) {
-    if (data.src) chrome.tabs.create({
-      url: data.src,
-      active: true,
-      index: this.index + 1
-    })
+    if (data.target.nodeName.toLowerCase() === "img" && data.target.src)
+      chrome.tabs.create({
+        url: data.target.src,
+        active: true,
+        index: this.index + 1
+      })
   },
 
   LinkToForegroundTab: function (data) {
-    if (isURL(data.selection)) data.href = data.selection;
-    if (data.href || data.newTabOnEmptyLink) chrome.tabs.create({
-      url: data.href,
+    let url = null;
+    if (isURL(data.textSelection)) url = data.textSelection;
+    else if (data.link && data.link.href) url = data.link.href;
+
+    if (url || data.newTabOnEmptyLink) chrome.tabs.create({
+      url: url,
       active: true,
       index: this.index + 1
     })
   },
 
   LinkToBackgroundTab: function (data) {
-    if (isURL(data.selection)) data.href = data.selection;
-    if (data.href || data.newTabOnEmptyLink) chrome.tabs.create({
-      url: data.href,
-      active: false,
-      index: this.index + 1
-    })
+    let url = null;
+    let index = this.index + 1;
+
+    if (isURL(data.textSelection)) url = data.textSelection;
+    else if (data.link && data.link.href) url = data.link.href;
+    // get tabs previously opened by this tab
+    chrome.tabs.query({
+      openerTabId: this.id
+    }, (tabs) => {
+      // if there are any previously opened tabs
+      if (tabs.length > 0) {
+        // get highest index and increment it
+        index = tabs.reduce((pre, cur) => pre.index > cur.index ? pre : cur).index + 1;
+      }
+      // if not then this is the first time a background tab gets opened
+      else {
+        let tabId = this.id;
+        // add "active tab changed" event once
+        chrome.tabs.onActivated.addListener(function handler () {
+          // get affected tabs
+          chrome.tabs.query({
+            openerTabId: tabId
+          }, (tabs) => {
+            // remove openerTabId by setting it to the tabs id
+            for (let tab of tabs) chrome.tabs.update(tab.id, {openerTabId: tab.id});
+          });
+          // remove listener
+          chrome.tabs.onActivated.removeListener(handler);
+        });
+      }
+
+      // open new tab
+      if (url || data.newTabOnEmptyLink) chrome.tabs.create({
+        url: url,
+        active: false,
+        index: index,
+        openerTabId: this.id
+      })
+    });
   },
 
   LinkToBookmark: function (data) {
-    if (isURL(data.selection)) data.href = data.selection;
-    if (data.href) chrome.bookmarks.create({
-      url: data.href,
-      title: new URL(data.href).hostname
+    let url = null, title = null;
+
+    if (isURL(data.textSelection))
+      url = data.textSelection;
+    else if (data.link && data.link.href) {
+      url = data.link.href;
+      title = data.link.title || data.link.textContent || data.target.title || null;
+    }
+
+    if (url) chrome.bookmarks.create({
+      url: url,
+      title: title || new URL(url).hostname
     });
   },
 
   SearchSelection: function (data) {
     chrome.tabs.create({
-      url: data.searchEngineURL + data.selection,
+      url: data.searchEngineURL + data.textSelection,
       active: true,
       index: this.index + 1
     })
@@ -345,5 +425,72 @@ let Actions = {
 
   PrintPreview: function () {
     browser.tabs.printPreview();
+  },
+
+  SaveScreenshot: function () {
+    chrome.tabs.captureVisibleTab((url) => {
+      // convert data uri to blob
+      let binary = atob(url.split(',')[1]),
+          array = [];
+      for (var i = 0; i < binary.length; i++) {
+        array.push(binary.charCodeAt(i));
+      }
+      // convert blob to object url
+      url = URL.createObjectURL(
+        new Blob([new Uint8Array(array)], {type: "image/png"})
+      );
+
+      chrome.downloads.download({
+        url: url,
+        filename: this.title + '.png',
+        saveAs: true
+      });
+    });
+  },
+
+  CopyTabURL: function () {
+    let input = document.createElement("textarea");
+    document.body.append(input);
+    input.value = this.url;
+    input.select();
+    document.execCommand("copy");
+    document.body.removeChild(input);
+  },
+
+  CopyTextSelection: function (data) {
+    let input = document.createElement("textarea");
+    document.body.append(input);
+    input.value = data.textSelection;
+    input.select();
+    document.execCommand("copy");
+    document.body.removeChild(input);
+  },
+
+  CopyImage: function (data) {
+    if (data.target.nodeName.toLowerCase() === "img" && data.target.src) {
+      // get type by file extension, default type is png
+      let fileType = "png", mimeType = "image/png";
+      if (data.target.src.split('.').pop().toLowerCase() === "jpg") {
+        fileType = "jpeg";
+        mimeType = "image/jpeg";
+      }
+      // load image
+      let image = new Image();
+          image.onload = () => {
+            let canvas = document.createElement('canvas');
+                canvas.width = image.naturalWidth;
+                canvas.height = image.naturalHeight;
+                // draw image to canvas
+                canvas.getContext("2d").drawImage(image, 0, 0);
+                // get image as blob
+                canvas.toBlob((blob) => {
+                  let fileReader = new FileReader();
+                      // convert blob to array buffer
+                      fileReader.onload = () => chrome.clipboard.setImageData(fileReader.result, fileType);
+                      fileReader.readAsArrayBuffer(blob);
+                }, mimeType);
+          };
+          image.src = data.target.src;
+    }
   }
 };
--- src/ui/js/options.js
+++ src/ui/js/options.js
@@ -110,6 +110,14 @@ function main () {
         onCollapseButton.call(button);
       }
 
+  // data management buttons
+  let resetButton = document.getElementById("resetButton");
+      resetButton.onclick = onResetButton;
+  let backupButton = document.getElementById("backupButton");
+      backupButton.onclick = onBackupButton;
+  let restoreButton = document.getElementById("restoreButton");
+      restoreButton.onchange = onRestoreButton;
+
   /**
    * on tab close or url change or refresh save data to storage
    * also propagate the new settings to all tabs
@@ -337,6 +345,73 @@ function onGestureInput () {
       input.setCustomValidity('');
     }
   }
+}
+
+
+/**
+ * merges the default config into the current config
+ * reloads the options page afterwards
+ **/
+function onResetButton () {
+  if (window.confirm(browser.i18n.getMessage('aboutResetNotificationConfirm')))
+    Core.getJsonFileAsObject(chrome.runtime.getURL("res/config.json"), (config) => {
+      // keep the reference by using assign
+      Object.assign(Config, config);
+      // reload option page to update the ui and save/propagate new data
+      window.location.reload(false);
+    });
+}
+
+
+/**
+ * creates a json file with the current config and prompts a download dialog
+ **/
+function onBackupButton () {
+  let url = URL.createObjectURL(
+        new Blob([JSON.stringify(Config, null, '  ')], {type: 'application/json'})
+      );
+  let date = new Date().toDateString();
+
+  chrome.downloads.download({
+    url: url,
+    filename: `${Manifest.name} ${Manifest.version} ${date}.json`,
+    saveAs: true
+  });
+}
+
+
+/**
+ * creates a json file with the current config and prompts a download dialog
+ **/
+function onRestoreButton (event) {
+  if (this.files[0].type === "application/json") {
+    let reader = new FileReader();
+        reader.onloadend = () => {
+          let restoredConfig = JSON.parse(reader.result),
+              count = 0;
+
+          // this will iterate through the current config and check if there is a corresponding value in the restore config
+          function updateDeep(target, source) {
+            for (let key in target) {
+              if (key in source) {
+                if (Core.isObject(target[key]) && Core.isObject(source[key]))
+                  updateDeep(target[key], source[key]);
+                else if (!Core.isObject(target[key]) && !Core.isObject(source[key])) {
+                  target[key] = source[key];
+                  count++;
+                }
+              }
+            }
+          }
+          updateDeep(Config, restoredConfig);
+
+          alert(browser.i18n.getMessage('aboutRestoreNotificationSuccess', count));
+          // reload option page to update the ui and save/propagate new data
+          window.location.reload(false);
+        }
+        reader.readAsText(this.files[0]);
+  }
+  else alert(browser.i18n.getMessage('aboutRestoreNotificationWrongFile'));
 }
 
 
