435196793de5cd0decb2cde08f4be58180829c0e
Robbendebiene <robbendebiene@mailbox.org>
2018-07-20 16:12:31 +0200

    enhanced extras ui code

--- src/ui/js/content/extras.js
+++ src/ui/js/content/extras.js
@@ -6,41 +6,47 @@ const SettingTemplates = window.top.document.getElementById("CommandSettings").c
 
 // apply values to input fields and add their event function
 const commandSelects = document.querySelectorAll(".command-select-field");
-      for (let select of commandSelects) {
-        const commandObject = getObjectPropertyByString(Config.Settings, select.dataset.hierarchy)[select.name];
-        select.title = browser.i18n.getMessage(`commandLabel${commandObject.command}`);
-
-        // mit after im css anzeigen
-        select.addEventListener('click', onCommandSelect);
-      }
+for (let commandSelect of commandSelects) {
+  const commandObject = getObjectPropertyByString(Config.Settings, commandSelect.dataset.hierarchy)[commandSelect.name];
+  commandSelect.title = browser.i18n.getMessage(`commandLabel${commandObject.command}`);
+  commandSelect.addEventListener('click', onCommandSelect);
+}
 
 
+/**
+ * Opens the command bar and overlay on command button click
+ **/
 function onCommandSelect () {
   const configObject = getObjectPropertyByString(Config.Settings, this.dataset.hierarchy);
 
   Overlay.open();
-  Overlay.onClick(closeAll);
-  CommandBar.open(configObject[this.name]);
-  CommandBar.onSelect((commandObject) => {
-    closeAll();
+  Overlay.onClick(closeCommandBar);
 
+  CommandBar.open(configObject[this.name]);
+  CommandBar.onCancel(closeCommandBar);
+
+  CommandBar.onSubmit((commandObject) => {
+    closeCommandBar();
+    // add popout animation for the updated command button
     this.addEventListener("animationend", (event) => {
       this.classList.remove("pop-out-animation");
     }, { once: true });
     this.classList.add("pop-out-animation");
-
-    const configObject = getObjectPropertyByString(Config.Settings, this.dataset.hierarchy);
+    // update the command button title
+    this.title = browser.i18n.getMessage(`commandLabel${commandObject.command}`);
+    // save command object to config
     configObject[this.name] = cloneObjectInto(
       commandObject,
       window.top
     );
-
-    this.title = browser.i18n.getMessage(`commandLabel${commandObject.command}`);
   });
-  CommandBar.onCancel(closeAll);
 }
 
-function closeAll () {
+
+/**
+ * Closes the command bar and overlay
+ **/
+function closeCommandBar () {
   Overlay.close();
   CommandBar.close();
 }
