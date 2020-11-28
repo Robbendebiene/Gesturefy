3a31bd585f1bfa7a0ef278be943a6a87b3164139
deniz <github@ledeniz.de>
2020-11-28 14:23:44 +0100

    rename blacklist to "exlcusions"
    as discussed here https://github.com/Robbendebiene/Gesturefy/pull/552#issuecomment-727553104

--- src/views/options/js/content/blacklist.js
+++ src/views/options/js/content/blacklist.js
@@ -7,73 +7,73 @@ ContentLoaded.then(main);
  * run code that depends on async resources
  **/
 function main () {
-  const blacklistContainer = document.getElementById('blacklistContainer');
-        blacklistContainer.dataset.noEntriesHint = browser.i18n.getMessage('blacklistHintNoEntries');
-  const blacklistForm = document.getElementById('blacklistForm');
-        blacklistForm.onsubmit = onFormSubmit;
-        blacklistForm.elements.urlPattern.placeholder = browser.i18n.getMessage('blacklistPlaceholderURL');
-        blacklistForm.elements.urlPattern.title = browser.i18n.getMessage('blacklistPlaceholderURL');
-        blacklistForm.elements.urlPattern.onchange = onInputChange;
-  // add existing blacklist entries
-  for (const urlPattern of Config.get("Blacklist")) {
-    const blacklistEntry = createBlacklistEntry(urlPattern);
-    blacklistContainer.appendChild(blacklistEntry);
+  const exclusionsContainer = document.getElementById('exclusionsContainer');
+        exclusionsContainer.dataset.noEntriesHint = browser.i18n.getMessage('exclusionsHintNoEntries');
+  const exclusionsForm = document.getElementById('exclusionsForm');
+        exclusionsForm.onsubmit = onFormSubmit;
+        exclusionsForm.elements.urlPattern.placeholder = browser.i18n.getMessage('exclusionsPlaceholderURL');
+        exclusionsForm.elements.urlPattern.title = browser.i18n.getMessage('exclusionsPlaceholderURL');
+        exclusionsForm.elements.urlPattern.onchange = onInputChange;
+  // add existing exclusions entries
+  for (const urlPattern of Config.get("Exclusions")) {
+    const exclusionsEntry = createExclusionsEntry(urlPattern);
+    exclusionsContainer.appendChild(exclusionsEntry);
   }
 }
 
 
 /**
- * Creates a blacklist entry html element by a given url pattern and returns it
+ * Creates a exclusions entry html element by a given url pattern and returns it
  **/
-function createBlacklistEntry (urlPattern) {
-  const blacklistEntry = document.createElement('li');
-        blacklistEntry.classList.add('bl-entry');
-        blacklistEntry.dataset.urlPattern = urlPattern;
-        blacklistEntry.onclick = onEntryClick;
+function createExclusionsEntry (urlPattern) {
+  const exclusionsEntry = document.createElement('li');
+        exclusionsEntry.classList.add('bl-entry');
+        exclusionsEntry.dataset.urlPattern = urlPattern;
+        exclusionsEntry.onclick = onEntryClick;
   const inputURLEntry = document.createElement('div');
         inputURLEntry.classList.add('bl-url-pattern');
         inputURLEntry.textContent = urlPattern;
   const deleteButton = document.createElement('button');
         deleteButton.type = "button";
         deleteButton.classList.add('bl-remove-button', 'icon-delete');
-  blacklistEntry.append(inputURLEntry, deleteButton);
-  return blacklistEntry;
+  exclusionsEntry.append(inputURLEntry, deleteButton);
+  return exclusionsEntry;
 }
 
 
 /**
- * Adds a given blacklist entry element to the blacklist ui
+ * Adds a given exclusions entry element to the exclusions ui
  **/
-function addBlacklistEntry (blacklistEntry) {
-  const blacklistContainer = document.getElementById('blacklistContainer');
+function addExclusionsEntry (exclusionsEntry) {
+  const exclusionsContainer = document.getElementById('exclusionsContainer');
   // append entry, hide it and move it out of flow to calculate its dimensions
-  blacklistContainer.prepend(blacklistEntry);
-  blacklistEntry.style.setProperty('visibility', 'hidden');
-  blacklistEntry.style.setProperty('position', 'absolute');
+  exclusionsContainer.prepend(exclusionsEntry);
+  exclusionsEntry.style.setProperty('visibility', 'hidden');
+  exclusionsEntry.style.setProperty('position', 'absolute');
   // calculate total entry height
-  const computedStyle = window.getComputedStyle(blacklistEntry);
-  const outerHeight = parseInt(computedStyle.marginTop) + blacklistEntry.offsetHeight + parseInt(computedStyle.marginBottom);
+  const computedStyle = window.getComputedStyle(exclusionsEntry);
+  const outerHeight = parseInt(computedStyle.marginTop) + exclusionsEntry.offsetHeight + parseInt(computedStyle.marginBottom);
 
   // move all entries up by one entry including the new one
-  for (const node of blacklistContainer.children) {
+  for (const node of exclusionsContainer.children) {
     node.style.setProperty('transform', `translateY(-${outerHeight}px)`);
     // remove ongoing transitions if existing
     node.style.removeProperty('transition');
   }
   // show new entry and bring it back to flow, which pushes all elements down by the height of one entry
-  blacklistEntry.style.removeProperty('visibility', 'hidden');
-  blacklistEntry.style.removeProperty('position', 'absolute');
+  exclusionsEntry.style.removeProperty('visibility', 'hidden');
+  exclusionsEntry.style.removeProperty('position', 'absolute');
 
   // trigger reflow
-  blacklistContainer.offsetHeight;
+  exclusionsContainer.offsetHeight;
 
-  blacklistEntry.addEventListener('animationend', (event) => {
+  exclusionsEntry.addEventListener('animationend', (event) => {
     event.currentTarget.classList.remove('bl-entry-animate-add');
   }, {once: true });
-  blacklistEntry.classList.add('bl-entry-animate-add');
+  exclusionsEntry.classList.add('bl-entry-animate-add');
 
   // move all entries down including the new one
-  for (const node of blacklistContainer.children) {
+  for (const node of exclusionsContainer.children) {
     node.addEventListener('transitionend', (event) => event.currentTarget.style.removeProperty('transition'), {once: true });
     node.style.setProperty('transition', 'transform 0.3s');
     node.style.removeProperty('transform');
@@ -82,14 +82,14 @@ function addBlacklistEntry (blacklistEntry) {
 
 
 /**
- * Removes a given blacklist entry element from the blacklist ui
+ * Removes a given exclusions entry element from the exclusions ui
  **/
-function removeBlacklistEntry (blacklistEntry) {
+function removeExclusionsEntry (exclusionsEntry) {
   // calculate total entry height
-  const computedStyle = window.getComputedStyle(blacklistEntry);
-  const outerHeight = parseInt(computedStyle.marginTop) + blacklistEntry.offsetHeight + parseInt(computedStyle.marginBottom);
+  const computedStyle = window.getComputedStyle(exclusionsEntry);
+  const outerHeight = parseInt(computedStyle.marginTop) + exclusionsEntry.offsetHeight + parseInt(computedStyle.marginBottom);
 
-  let node = blacklistEntry.nextElementSibling;
+  let node = exclusionsEntry.nextElementSibling;
   while (node) {
     node.addEventListener('transitionend', (event) => {
       event.currentTarget.style.removeProperty('transition');
@@ -99,27 +99,27 @@ function removeBlacklistEntry (blacklistEntry) {
     node.style.setProperty('transform', `translateY(-${outerHeight}px)`);
     node = node.nextElementSibling;
   }
-  blacklistEntry.addEventListener('animationend', (event) => event.currentTarget.remove(), {once: true });
-  blacklistEntry.classList.add('bl-entry-animate-remove');
+  exclusionsEntry.addEventListener('animationend', (event) => event.currentTarget.remove(), {once: true });
+  exclusionsEntry.classList.add('bl-entry-animate-remove');
 }
 
 
 /**
  * Handles the url pattern submit event
- * Adds the new url pattern to the config and calls the blacklist entry create function
+ * Adds the new url pattern to the config and calls the exclusions entry create function
  **/
 function onFormSubmit (event) {
   event.preventDefault();
   // remove spaces and cancel the function if the value is empty
   const urlPattern = this.elements.urlPattern.value.trim();
   if (!urlPattern) return;
-  // create and add entry to the blacklist
-  const blacklistEntry = createBlacklistEntry(urlPattern);
-  addBlacklistEntry(blacklistEntry);
+  // create and add entry to the exclusions
+  const exclusionsEntry = createExclusionsEntry(urlPattern);
+  addExclusionsEntry(exclusionsEntry);
   // add new url pattern to the beginning of the array
-  const blacklistArray = Config.get("Blacklist");
-        blacklistArray.unshift(urlPattern);
-  Config.set("Blacklist", blacklistArray);
+  const exclusionsArray = Config.get("Exclusions");
+        exclusionsArray.unshift(urlPattern);
+  Config.set("Exclusions", exclusionsArray);
   // clear input field
   this.elements.urlPattern.value = '';
 }
@@ -130,33 +130,33 @@ function onFormSubmit (event) {
  * Marks the field as invalide if the entry already exists
  **/
 function onInputChange () {
-  if (Config.get("Blacklist").indexOf(this.value.trim()) !== -1) {
-    this.setCustomValidity(browser.i18n.getMessage('blacklistNotificationAlreadyExists'));
+  if (Config.get("Exclusions").indexOf(this.value.trim()) !== -1) {
+    this.setCustomValidity(browser.i18n.getMessage('exclusionsNotificationAlreadyExists'));
   }
   else if (this.validity.customError) this.setCustomValidity('');
 }
 
 
 /**
- * Handles the blacklist entry click
- * Calls the remove blacklist entry function on remove button click and removes it from the config
+ * Handles the exclusions entry click
+ * Calls the remove exclusions entry function on remove button click and removes it from the config
  **/
 function onEntryClick (event) {
   // if delete button received the click
   if (event.target.classList.contains('bl-remove-button')) {
-    removeBlacklistEntry(this);
+    removeExclusionsEntry(this);
 
-    const blacklistForm = document.getElementById('blacklistForm');
+    const exclusionsForm = document.getElementById('exclusionsForm');
     // remove input field invaldility if it was previously a duplicate
-    if (this.dataset.urlPattern === blacklistForm.elements.urlPattern.value.trim()) {
-      blacklistForm.elements.urlPattern.setCustomValidity('');
+    if (this.dataset.urlPattern === exclusionsForm.elements.urlPattern.value.trim()) {
+      exclusionsForm.elements.urlPattern.setCustomValidity('');
     }
-    const blacklistArray = Config.get("Blacklist");
+    const exclusionsArray = Config.get("Exclusions");
     // remove url pattern from array
-    const index = blacklistArray.indexOf(this.dataset.urlPattern);
+    const index = exclusionsArray.indexOf(this.dataset.urlPattern);
     if (index !== -1) {
-      blacklistArray.splice(index, 1);
-      Config.set("Blacklist", blacklistArray);
+      exclusionsArray.splice(index, 1);
+      Config.set("Exclusions", exclusionsArray);
     }
   }
 }
